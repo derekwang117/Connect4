6fe9c9d6bdc160f64afdefbde0b27c2fcda76687
Derek Wang <toderekwang@gmail.com>
2022-01-16 19:34:04 -0500

    implemented hashing each board state with Zobrist hashing
    implemented zobrist hashing
    called whenever change boardstate (drop, undo)

--- main.py
+++ main.py
@@ -23,6 +23,22 @@ class Connect4Board:
         self.empty_flags = np.ones(self.column_count, dtype=bool)
         self.col_flag = np.zeros(self.column_count, dtype=int)
 
+        self.ZobristTable = np.random.randint(2147483647, 9223372036854775807,
+                                              size=(self.column_count, self.row_count, 2), dtype=np.int64)
+        self.hash = self.compute_hash()
+        print(self.hash)
+
+    def compute_hash(self):
+        hash_val = 0
+        for i in range(self.column_count):
+            for j in range(self.row_count):
+                if self.board[i][j]:
+                    hash_val ^= self.ZobristTable[i][j][self.board[i][j] - 1]
+        return hash_val
+
+    def update_hash(self, col, row, changed):
+        self.hash ^= self.ZobristTable[col][row][changed - 1]
+
     def print_board(self):
         print(np.rot90(self.board))
         print()
@@ -34,12 +50,18 @@ class Connect4Board:
             self.empty_flags[col] = False
         self.col_flag[col] += 1
 
+        self.update_hash(col, open_index, player)
+        print(self.hash)
+
     def undo_drop(self, col, player):
         last_index = np.where(self.board[col] == player)[0][-1]
         self.board[col][last_index] = 0
         if last_index == self.row_count - 1:
             self.empty_flags[col] = True
         self.col_flag[col] -= 1
+
+        self.update_hash(col, last_index, player)
+        print(self.hash)
 
     def can_drop(self, col):
         return self.empty_flags[col]
@@ -133,7 +155,6 @@ class Connect4Board:
                 best_val = move_val
                 best_move = pos_move
 
-        print(best_val)
         print(best_move)
         return best_move
 
@@ -149,7 +170,7 @@ def fill_board(board, move_sequence):
 def test(board):
     start_time = round(time.time() * 1000)
 
-    bot_n = 1
+    bot_n = 2
     x = board.find_best_move(bot_n)
     board.drop(x, bot_n)
     board.print_board()
@@ -161,15 +182,11 @@ def test(board):
 def main():
     c4_game = Connect4Board()
 
-    # fill_board(c4_game, "1133")
-    c4_game.drop(1, 1)
-    c4_game.drop(1, 2)
-    c4_game.drop(3, 1)
-    c4_game.drop(3, 2)
+    fill_board(c4_game, "222")
     c4_game.print_board()
 
     # zobrist hashing and multithreading
-    test(c4_game)
+    #test(c4_game)
 
 
 if __name__ == '__main__':
